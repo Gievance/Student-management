e447888018ce29e5616ce24329a68b65bb089d07
Alex <779850604@qq.com>
2020-07-24 11:52:53 +0800

    学生管理系统-Version 4(完成版）

--- Scanners.java
+++ Scanners.java
@@ -2,6 +2,6 @@ package com.version1;
 
 import java.io.IOException;
 
-public interface Scanners<T> {
+public interface Scanners<T>{
     T scanner() throws InterruptedException, IOException, ClassNotFoundException;
 }
